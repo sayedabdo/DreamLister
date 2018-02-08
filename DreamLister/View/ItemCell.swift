442afde17cf08cc1c2e73d3b6a5d3cc5d45908d9
Sayed Abdo <sayedabdo@Sayeds-Mac.local>
2018-02-08 13:16:02 -0800

    Save Item :) :)

---
+++ DreamLister/View/ItemCell.swift
@@ -0,0 +1,26 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Sayed Abdo on 2/8/18.
+//  Copyright © 2018 sayedAbdo. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+    func configureCell(item: Item) {
+        
+        title.text = item.title
+        price.text = "$\(item.price)"
+        details.text = item.details
+        thumb.image = item.toImage?.image as? UIImage
+        
+    }
+}
